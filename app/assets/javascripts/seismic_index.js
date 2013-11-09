10e1b3f2ef744aeaa5f1501177b2817c2d19bfca
Chris <c.coe.12@aberdeen.ac.uk>
2013-11-09 19:49:06 +0000

    Google maps stuff - Make map variable global - Add a map movement test

--- app/assets/javascripts/seismic_index.js
+++ app/assets/javascripts/seismic_index.js
@@ -1,2 +1,24 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+// Set global variable for map.
+// Global scope required to update from timeout.
+var map;
+
+function updateEarthquakes() {
+    // Generate random values for now
+    var lat = Math.random() * 180 - 90;
+    var long = Math.random() * 360 - 180;
+
+    // Set up new maps location
+    var newLocation = new google.maps.LatLng(lat, long);
+
+    // Move map
+    map.panTo(newLocation);
+}
+
+$(function () {
+        // Set update to run every x seconds
+        setInterval(updateEarthquakes, 5000)
+    }
+);
