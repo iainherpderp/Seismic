fa119c55d0497dd228d181ca04abe78a5a7ceb8a
Liam Ramm <liamramm@yahoo.co.uk>
2013-11-10 21:14:39 +0000

    First AJAX test

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Seismic::Application.routes.draw do
+	
+	resources :SeismicIndex do
+		
+		member do
+			get 'fileContent'
+		end
+	end
+
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
