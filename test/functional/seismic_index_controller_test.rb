0ba698619c561b24e6bd476b1ffb54588a0986b5
Liam Ramm <liamramm@yahoo.co.uk>
2013-11-11 19:47:19 +0000

    starting Unit tests

--- test/functional/seismic_index_controller_test.rb
+++ test/functional/seismic_index_controller_test.rb
@@ -3,5 +3,12 @@ require 'test_helper'
 class SeismicIndexControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
-  # end
+  # end	
+	test "should return a Hash" do
+		get "/"
+		assert_response :success
+		assert @quake_data.class == "Hash", "did not not return a Hash: #{@quake_data.class}"
+	end
+  
+	
 end
