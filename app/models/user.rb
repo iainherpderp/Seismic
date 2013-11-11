a2b2eef1ea701f08fe7d66baf00e68c8b9bd5177
Iain Macdonald <iain.j.macdonald.12@aberdeen.ac.uk>
2013-11-11 14:32:17 +0000

    edited gemfile to include bcrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,33 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :isadmin, :password_hash, :password_salt, :username
+
+	attr_accessible :username, :email, :password_hash, :password_salt, :password, :password_confirmation
+ 
+	attr_accessor :password,:password_confirmation
+
+	before_save :encrypt_password
+  
+	validates_confirmation_of :password  
+  
+	validates_presence_of :password, :on => :create  
+  
+	validates_presence_of :username
+  
+	validates_presence_of :email  
+  
+	validates_uniqueness_of :username
+  
+	validates_uniqueness_of :email 
+	
+	def encrypt_password
+
+		if password.present?
+
+			self.password_salt = BCrypt::Engine.generate_salt
+
+			self.password_hash = BCrypt::Engine.hash_secret(password, password_salt)
+
+		end  
+
+	end
+  
 end
