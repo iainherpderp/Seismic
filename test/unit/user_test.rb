3bf9e4c59ad3bffde7fb30f8d09eaae6fc3d8251
Chris <c.coe.12@aberdeen.ac.uk>
2013-11-14 17:19:02 +0000

    One more test or two

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -5,7 +5,22 @@ class UserTest < ActiveSupport::TestCase
   #   assert true
   # end
   test "should not save user without password" do
-    post = User.new
+    # Written by Liam
+    post = User.new(username: 'test1')
     assert !post.save, "Saved the user without a password"
   end
+
+  test "should not save user without username" do
+    # Written by Chris
+    post = User.new(password: 'test', password_confirmation: 'test')
+    assert !post.save, "Saved the user without a username"
+  end
+
+  test "should not save user with mismatched password confirmation" do
+    # Written by Chris
+    post = User.new(username: 'test2', password: 'test', password_confirmation: 'notest')
+    assert !post.save, "Saved the user without a matched password confirmation"
+  end
+
+
 end
